119848f6033862a6994f702f58e5c32ce427a638
DamienFoulon <foulon.damien.pro@gmail.com>
2023-01-23 22:51:14 +0100

    Add dota2 matches and leagues controllers

--- server/controllers/api/dota2.js
+++ server/controllers/api/dota2.js
@@ -3,9 +3,41 @@ export function dota2(req, res) {
 }
 
 export function dota2Matches(req, res) {
-    res.send('This is Dota 2 Matches API section');
+    let sort = data.query.sort
+
+    let dota2Matches = pandascore(
+        `https://api.pandascore.co/dota2/matches?token=${process.env.PANDASCORE_TOKEN}`
+    )
+
+    switch (sort) {
+        default:
+            dota2Matches.then((data) => {
+                res.json(data)
+            })
+            break
+
+        case 'upcoming':
+            dota2Matches.then((data) => {
+                res.json(
+                    data
+                        .sort((a, b) => {
+                            return new Date(a.begin_at) - new Date(b.begin_at)
+                        })
+                        .filter((match) => {
+                            return new Date(match.begin_at) > new Date()
+                        })
+                )
+            })
+            break
+    }
 }
 
 export function dota2Leagues(req, res) {
-    res.send('This is Dota 2 League API section');
+    let dota2Leagues = pandascore(
+        `https://api.pandascore.co/dota2/leagues?token=${process.env.PANDASCORE_TOKEN}`
+    )
+
+    dota2Leagues.then((data) => {
+        res.json(data)
+    })
 }
