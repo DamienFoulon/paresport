ba6a45658a3961818512333cb4d00c8a70bfbfbe
DamienFoulon <44929858+DamienFoulon@users.noreply.github.com>
2023-07-25 10:40:47 +0200

    Set cronjobs to every 25mn

--- server/scheduled/removeBlacklistTokens.js
+++ server/scheduled/removeBlacklistTokens.js
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function scheduledRemoveBlacklistTokens() {
-    cron.schedule('*/30 * * * *', async () => {
+    cron.schedule('*/25 * * * *', async () => {
         const tokens = await prisma.blacklist_Tokens.findMany().then((tokens) => {
             tokens.forEach(async (token) => {
                 if(token.remainingTime < new Date()) {
